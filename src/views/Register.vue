5b1d240f66930d3fbba988d8abbdc51d2c4f95d9
Lybia PC001 <nqhuy250901@gmail.com>
2022-12-14 17:34:05 +0700

    add ant design

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 
+import { reactive } from 'vue';
+import { Button } from 'ant-design-vue'
+const ButtonGroup = Button.Group
+
+interface SignUp {
+    email: string,
+    phone_number: string,
+    username: string,
+    password: string
+}
+
+const state: SignUp = reactive({ email: '', phone_number: '', username: '', password: '' })
+
 </script>
 
 <template>
+
+    <a-button type="primary">Primary Button</a-button>
 </template>
